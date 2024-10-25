ca18e0c188d582e5693bd6b13ac077828b84756d
yolvinruiz <yolvitoruiz@gmail.com>
2024-10-25 10:12:29 -0500

    cambios 6

---
+++ ivanshoes/paginaverpedidos.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ivanshoes
+{
+    /// <summary>
+    /// Lógica de interacción para paginaverpedidos.xaml
+    /// </summary>
+    public partial class paginaverpedidos : Page
+    {
+        public paginaverpedidos()
+        {
+            InitializeComponent();
+        }
+    }
+}
